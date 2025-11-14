177e5279f88a0874c49ca07481989105f9e78d11
willbrown <will.j.brown.2@gmail.com>
2025-11-13 20:29:04 -0700

    move to new computer

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,3 @@
 mod health_check;
 mod subscriptions;
 
-pub use health_check::*;
-pub use subscriptions::*;
--- src/routes/subscriptions.rs
+++
@@ -1,4 +0,0 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_web::web::Form;
-use actix_web::dev::Server;
-use std::net::TcpListener;
