1705d83ba1a575230a636f2ef3a4a81dcea3e9d8
willbrown <will.j.brown.2@gmail.com>
2025-11-18 19:43:17 -0700

    get sink setup

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,12 +5,24 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::sync::LazyLock;
 
 static TRACING: LazyLock<()> =  LazyLock::new(|| {
-    let subscriber = get_subscriber(
-        "test".to_string(),
-        "debug".to_string(),
-        std::io::stdout,
-    );
-    init_subscriber(subscriber);
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            default_filter_level,
+            subscriber_name,
+            std::io::stdout,
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink,
+        );
+        init_subscriber(subscriber);
+    }
 });
 pub struct TestApp {
     pub address: String,
