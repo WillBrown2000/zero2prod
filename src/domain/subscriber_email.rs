343f531705de87492844ecc6a5c6403e676698c1
willbrown <will.j.brown.2@gmail.com>
2025-11-24 10:15:39 -0700

    get all tests passing

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,10 +22,24 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use claims::{assert_err, assert_ok};
-    use crate::domain::subscriber_email::SubscriberEmail;
     use fake::Fake;
     use fake::faker::internet::en::SafeEmail;
+    use quickcheck::Gen;
+    use crate::domain::subscriber_email::SubscriberEmail;
+    use quickcheck_macros::quickcheck;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
+    #[derive(Debug, Clone)]
+    struct ValidateEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidateEmailFixture {
+        fn arbitrary(g: &mut Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
     #[test]
     fn empty_strings_is_rejected() {
         let email = "".to_string();
@@ -43,9 +57,8 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = SafeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
+    #[quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidateEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
     }
 }
