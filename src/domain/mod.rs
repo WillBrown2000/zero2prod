fd3211765be13c09549b52a28a08b79e6ca81fe5
willbrown <will.j.brown.2@gmail.com>
2025-11-25 12:26:13 -0700

    basic email tests

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -3,3 +3,4 @@ mod subscriber_name;
 mod new_subscriber;
 pub use new_subscriber::NewSubscriber;
 pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
--- src/email_client.rs
+++ src/email_client.rs
@@ -33,3 +33,36 @@ impl EmailClient {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::time::Duration;
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::{Fake, Faker};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Duration::new(u64::MAX, 0));
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(10..20).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content).await;
+
+    }
+}
