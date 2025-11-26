1076ded246d748f042de80654218ba275fb3b817
willbrown <will.j.brown.2@gmail.com>
2025-11-26 08:31:53 -0700

    add check to email tests

--- src/email_client.rs
+++ src/email_client.rs
@@ -72,9 +72,25 @@ mod tests {
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::Fake;
     use secrecy::SecretString;
-    use wiremock::matchers::any;
+    use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
 
+    struct SendEmailBodyMatcher;
+
+    impl wiremock::Match for SendEmailBodyMatcher {
+        fn matches(&self, request: &wiremock::Request) -> bool {
+            let result: Result<serde_json::Value, _> = serde_json::from_slice(&request.body);
+            if let Ok(body) = result {
+                dbg!(&body);
+                body.get("from").is_some()
+                && body.get("to").is_some()
+                && body.get("subject").is_some()
+                && body.get("html_content").is_some()
+            } else {
+                false
+            }
+        }
+    }
     #[tokio::test]
     async fn send_email_fires_a_request_to_base_url() {
         let mock_server = MockServer::start().await;
@@ -86,8 +102,12 @@ mod tests {
             SecretString::new("secret".to_string().into_boxed_str()),
         );
 
-        Mock::given(any())
-            .respond_with(ResponseTemplate::new(200))
+        Mock::given(header_exists("X-Postmark-Server-Token"))
+            .and(header("Content-Type", "application/json"))
+            .and(path("/email"))
+            .and(method("POST"))
+            .and(SendEmailBodyMatcher)
+            .respond_with(ResponseTemplate::new(200).set_body_string("OK"))
             .expect(1)
             .mount(&mock_server)
             .await;
